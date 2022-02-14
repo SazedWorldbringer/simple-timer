238dd42ed4ce28084aa0db35d6b993186a1a3ad3
Atharva Pardeshi <atharva.exe@gmail.com>
2022-02-14 22:36:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let timeBegan = null; // has the clock started yet?
+let timeStopped = null; // at what time was the timer stopped?
+let stoppedDuration = 0; // how long was the timer stopped?
+let startInterval = null; // needed to stop the startInterval() method
+let flag = false; // to control the start/stop of the timer
+
+const timerContainer = document.getElementsByClassName("timer-container");
+
+timerContainer.addEventListener("click", function () {
+  if (!flag) {
+    startTimer();
+    flag = true;
+  } else {
+    stopTimer();
+    flag = false;
+  }
+});
+
+function startTimer() {
+  if (timeBegan === null) {
+    timeBegan = new Date();
+  }
+  if (timeStopped !== null) {
+    stoppedDuration += new Date() - timeStopped;
+  }
+  startInterval = setInterval(clockRunning, 10);
+}
+
+function stopTimer() {
+  timeStopped = new Date();
+  clearInterval(startInterval);
+}
+
+function clockRunning() {
+  let currentTime = new Date();
+  let timeElapsed = new Date(currentTime - timeBegan - stoppedDuration);
+
+  let minutes = timeElapsed.getUTCMinutes();
+  let seconds = timeElapsed.getUTCSeconds();
+  let milliseconds = timeElapsed.getUTCMilliseconds();
+
+  milliseconds = Math.floor(milliseconds / 10);
+
+  document.getElementById("timer-display").innerHTML =
+    (minutes = minutes < 10 ? "0" + minutes : minutes) +
+    ":" +
+    (seconds = seconds < 10 ? "0" + seconds : seconds) +
+    ":" +
+    (milliseconds = milliseconds < 10 ? "0" + milliseconds : milliseconds);
+}
